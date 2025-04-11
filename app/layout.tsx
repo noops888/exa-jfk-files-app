fceeddb1690c06bc6c6c71a2554256fd19c5e452
noops888 <58652273+noops888@users.noreply.github.com>
2025-04-11 10:01:48 +0300

    Update layout.tsx to fix iOS viewport bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body
         className={`${abcdDiatype.variable} ${reckless.variable} antialiased`}
